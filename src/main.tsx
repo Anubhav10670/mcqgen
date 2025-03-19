455a529b1567c76a4a4229ee85624866625f2122
Anubhav10670 <142131004+Anubhav10670@users.noreply.github.com>
2025-03-19 13:18:58 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-const apikey = "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"; //api key entered , change in future if needed, also dont forgot to change the model name.
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
